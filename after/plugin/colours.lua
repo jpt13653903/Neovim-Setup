8cc7a328bfad45d158ffcc54ba43c5345db92fb4
John-Philip Taylor <jpt13653903@gmail.com>
2023-11-05 01:11:58 +0200

    Conditionals are now also not italic

--- after/plugin/colours.lua
+++ after/plugin/colours.lua
@@ -9,11 +9,37 @@ colour.setup({
     percentage = 0.2,
   },
   styles = {
-    comments = {},
+    comments     = {},
+    conditionals = {},
   },
+  -- highlight_overrides = {
+  --   all = function(colors)
+  --     return {
+  --       Constant = { fg = C.teal }, -- (preferred) any constant
+  --       Number   = { fg = C.teal, style = O.styles.numbers or {} }, --   a number constant: 234, 0xff
+  --       Boolean  = { fg = C.teal, style = O.styles.booleans or {} }, --  a boolean constant: TRUE, false
+
+  --       String = { fg = C.lavender, style = O.styles.strings or {} }, -- a string constant: "this is a string"
+
+  --       Identifier = { fg = C.sapphire, style = O.styles.variables or {} }, -- (preferred) any variable name
+
+  --       Statement   = { fg = C.blue }, -- (preferred) any statement
+  --       Conditional = { fg = C.blue, style = O.styles.conditionals or {} }, --  if, then, else, endif, switch, etc.
+  --       Repeat      = { fg = C.blue, style = O.styles.loops or {} }, --   for, do, while, etc.
+  --       Keyword     = { fg = C.blue, style = O.styles.keywords or {} }, --  any other keyword
+
+	-- 	    PreProc = { fg = C.peach }, -- (preferred) generic Preprocessor
+
+  --       StorageClass = { fg = C.green }, -- static, register, volatile, etc.
+  --       Structure    = { fg = C.green }, --  struct, union, enum, etc.
+  --       Type         = { fg = C.green, style = O.styles.types or {} }, -- (preferred) int, long, char, etc.
+  --     }
+  --   end
+  -- },
 })
 
 vim.opt.background = 'dark'
 vim.cmd.colors('catppuccin')
+-- vim.cmd.colors('petrel-jpt')
 vim.cmd.syntax('enable')
 
