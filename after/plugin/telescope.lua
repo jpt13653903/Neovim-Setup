9df7ad25088628fa97536f285c7b5ed720203bc5
John-Philip Taylor <jpt13653903@gmail.com>
2023-11-05 11:26:34 +0200

    Changed the grep-based telescope shortcuts to " *" and " /"

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,11 +1,11 @@
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.grep_string, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-vim.keymap.set('n', '<leader>fm', builtin.marks, {})
-vim.keymap.set('n', '<leader>fo', builtin.oldfiles, {})
+vim.keymap.set('n', '<leader>fo', builtin.oldfiles,   {})
+vim.keymap.set('n', '<leader>fm', builtin.marks,      {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers,    {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags,  {})
 
-vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>*', builtin.grep_string, {})
+vim.keymap.set('n', '<leader>/', builtin.live_grep,   {})
 
