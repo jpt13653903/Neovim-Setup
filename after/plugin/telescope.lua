ee7c3605fb6c8640fea2e64e656ef4bc5ba86acd
John-Philip Taylor <John-PhilipT@reutech.co.za>
2023-11-06 09:29:43 +0200

    Added a git_files keymap to Telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,7 @@
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.git_files,  {})
 vim.keymap.set('n', '<leader>fo', builtin.oldfiles,   {})
 vim.keymap.set('n', '<leader>fm', builtin.marks,      {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers,    {})
