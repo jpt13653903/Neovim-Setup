b6ccd47fd3d15ca7ac6c9887cbc747d4736a2896
John-Philip Taylor <John-PhilipT@reutech.co.za>
2023-11-10 15:43:54 +0200

    Got a basic VHDL Treesitter working

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -2,7 +2,7 @@ local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
 
 parser_config.vhdl = {
   install_info = {
-    url = "https://github.com/alemuller/tree-sitter-vhdl.git",
+    url = "https://github.com/jpt13653903/tree-sitter-vhdl.git",
     files = { 'src/parser.c' },
     branch = 'main',
     generate_requires_npm = false, -- if stand-alone parser without npm dependencies
@@ -14,7 +14,6 @@ parser_config.vhdl = {
 local treesitter = require('nvim-treesitter.configs')
 
 treesitter.setup {
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = {
     'arduino',
     'bash',
@@ -63,20 +62,11 @@ treesitter.setup {
     'yaml',
   },
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
-
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
 
   highlight = {
     enable = true,
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
 }
