0ca79ed0a3c59006910daf854caec8411a7159c0
John-Philip Taylor <John-PhilipT@reutech.co.za>
2023-11-15 13:31:10 +0200

    Automatically fold JSON files to level 1

--- after/lua/config/autocmd.lua
+++ after/lua/config/autocmd.lua
@@ -1,7 +1,10 @@
--- Automatically update file when changed on disk
+-- Update buffer when file changed on disk
 vim.opt.autoread = true
 vim.cmd.autocmd('FocusGained * silent! checktime')
 
--- Automatically remove trailing white-space on save
+-- Remove trailing white-space on save
 vim.cmd.autocmd('BufWrite * silent! %s/\\s\\+$//g')
+
+-- Fold JSON files to level 1
+vim.cmd.autocmd('BufRead *.json,*.json5 silent! set foldlevel=1')
 
