65e23813053724922e59bd658129ab5c6d2321fc
John-Philip Taylor <John-PhilipT@reutech.co.za>
2023-12-04 18:35:53 +0200

    Added C/C++ conversions to/from KnR and Allman styles

---
+++ after/ftplugin/cpp.lua
@@ -0,0 +1,16 @@
+vim.api.nvim_create_user_command('StyleKnR', function()
+    pcall(vim.cmd, [[:%s/^\(\s\+.*)\)\(\s*\/\/.*\)\?\n\(\s\+\)\({.*\)/\1\4\2/g]])
+    pcall(vim.cmd, [[:%s/^\(\s\+.*else\)\(\s*\/\/.*\)\?\n\(\s\+\)\({.*\)/\1\4\2/g]])
+    pcall(vim.cmd, [[:%s/}\s*\n\s*else/}else/g]])
+    pcall(vim.cmd, [[:%s/\(if\|for\|while\|do\)\s*(/\1(/g]])
+    vim.cmd.nohlsearch()
+end, {})
+
+vim.api.nvim_create_user_command('StyleAllman', function()
+    pcall(vim.cmd, [[:%s/^\(\s\+\)\(.*\))\s*{\(\s*\/\/.*\)\?$/\1\2)\3\r\1{/g]])
+    pcall(vim.cmd, [[:%s/^\(\s\+\)}\s*else/\1}\r\1else/g]])
+    pcall(vim.cmd, [[:%s/^\(\s\+\)else\s*{\(\s*\/\/.*\)\?/\1else\2\r\1{/g]])
+    pcall(vim.cmd, [[:%s/\(if\|for\|while\|do\)\s*(/\1 (/g]])
+    vim.cmd.nohlsearch()
+end, {})
+
