bb1fafb8efb10c303de1522d5c3d9b1b7f718ab0
John-Philip Taylor <John-PhilipT@reutech.co.za>
2023-12-11 16:30:28 +0200

    KnR <-> Allman conversion: added space after "switch"

--- after/ftplugin/c.lua
+++ after/ftplugin/c.lua
@@ -6,7 +6,7 @@ vim.api.nvim_create_user_command('StyleKnR', function()
     -- Pull up the "else" after a closing brace
     pcall(vim.cmd, [[:%s/^\(\s*}\)\s*\n\s*else/\r\1else/g]])
     -- Remove spaces between control statements and the bracket
-    pcall(vim.cmd, [[:%s/\(if\|for\|while\|do\)\s*(/\1(/g]])
+    pcall(vim.cmd, [[:%s/\(if\|for\|while\|do\|switch\)\s*(/\1(/g]])
     -- Pull up braces for class and struct definitions, but not functions
     pcall(vim.cmd, [[:%s/\(.*\(struct\|class\).\{-}\)\(\s*\/\/.*\)\?$\n\s*{\s*$/\1{\3/g]])
     vim.cmd.nohlsearch()
@@ -20,7 +20,7 @@ vim.api.nvim_create_user_command('StyleAllman', function()
     -- Drop the opening brace after the "else"
     pcall(vim.cmd, [[:%s/^\(\s\+\)else\s*{\(\s*\/\/.*\)\?/\1else\2\r\1{/g]])
     -- Add space between the control statement and the bracket
-    pcall(vim.cmd, [[:%s/\(if\|for\|while\|do\)\s*(/\1 (/g]])
+    pcall(vim.cmd, [[:%s/\(if\|for\|while\|do\|switch\)\s*(/\1 (/g]])
     -- Make sure that all opening braces are on their own line (structs, classes, etc.)
     pcall(vim.cmd, [[:%s/^\(\s*\)\(\S.*\){\(\s*\/\/.*\)\?/\1\2\3\r\1{/g]])
     -- Remove empty lines above closing braces
