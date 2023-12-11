5d91b2cd0248331912788a8993d6ba430115d673
John-Philip Taylor <John-PhilipT@reutech.co.za>
2023-12-11 16:34:05 +0200

    Added the "Prettyfy" funciton to the json5 filetype

---
+++ after/ftplugin/json5.lua
@@ -0,0 +1,10 @@
+vim.api.nvim_create_user_command('Prettyfy', function()
+    pcall(vim.cmd, [[:%s/{/{\r/g]])
+    pcall(vim.cmd, [[:%s/}/\r}/g]])
+    pcall(vim.cmd, [[:%s/,/,\r/g]])
+    pcall(vim.cmd, [[:%s/\s\+$//g]])
+    pcall(vim.cmd, [[:%s/\n\+/\r/g]])
+    vim.cmd.nohlsearch()
+    vim.cmd.normal('ggVG=')
+end, {})
+
