d8c7e37b26436f6928cc11d967d1d072a75926db
John-Philip Taylor <jpt13653903@gmail.com>
2024-06-22 23:59:44 +0200

    Made the GUI 5% transparent

--- after/lua/config/autocmd.lua
+++ after/lua/config/autocmd.lua
@@ -11,3 +11,6 @@ vim.cmd.autocmd('BufRead *.json,*.json5 silent! set foldlevel=1')
 -- Disable folding in telescope results
 vim.cmd.autocmd('FileType TelescopeResults setlocal nofoldenable')
 
+-- Run commands that are only available after Qt attaches
+vim.cmd.autocmd('UIEnter * silent! GuiWindowOpacity 0.95')
+
