e0291014d207484d2c9e4e9733f9ee2e71059f17
John-Philip Taylor <John-PhilipT@reutech.co.za>
2024-06-25 18:37:39 +0200

    Tuned the Lualine settings

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,11 +1,20 @@
 vim.opt.showmode = false
 
+local custom_catppuccin = require "catppuccin.utils.lualine"()
+
+custom_catppuccin.normal  .a.bg = '#7091C4'
+custom_catppuccin.insert  .a.bg = '#8AB886'
+custom_catppuccin.command .a.bg = '#D49B77'
+custom_catppuccin.visual  .a.bg = '#BB9BE0'
+
+custom_catppuccin.normal  .c.bg = '#1e1e2e'
+custom_catppuccin.inactive.c.bg = '#181824'
+custom_catppuccin.normal  .c.fg = '#A1A8BF'
+
 require('lualine').setup {
     options = {
         icons_enabled = true,
-        theme = 'auto',
-        -- component_separators = { left = '', right = ''},
-        -- section_separators = { left = '', right = ''},
+        theme = custom_catppuccin,
         component_separators = { left = '', right = ''},
         section_separators = { left = '', right = ''},
         disabled_filetypes = {
@@ -22,9 +31,9 @@ require('lualine').setup {
         }
     },
     sections = {
-        lualine_a = {'mode'},
-        lualine_b = {},
-        lualine_c = {'filename'},
+        lualine_a = {{'mode', icons_enabled = false }},
+        lualine_b = {{ 'filename', path = 1 }},
+        lualine_c = {},
         lualine_x = {'encoding', { 'fileformat', symbols = { unix = 'Unix', dos = 'Dos', mac = 'Mac' } }, 'filetype'},
         lualine_y = {'progress'},
         lualine_z = {'location'}
@@ -32,7 +41,7 @@ require('lualine').setup {
     inactive_sections = {
         lualine_a = {},
         lualine_b = {},
-        lualine_c = {'filename'},
+        lualine_c = {{ 'filename', path = 1 }},
         lualine_x = {'location'},
         lualine_y = {},
         lualine_z = {}
@@ -46,9 +55,13 @@ require('lualine').setup {
                 active   = { fg = '#A1DA92', bg = '#424456' },
                 inactive = { fg = '#444659', bg = '#1A1A27' },
             },
+            symbols = {
+                modified = "'",
+            },
         }},
     },
     winbar = {},
     inactive_winbar = {},
     extensions = {}
 }
+
