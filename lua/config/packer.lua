bab723226ee02a61c326f6d91c478bb0e4970496
John-Philip Taylor <John-PhilipT@reutech.co.za>
2024-06-28 09:59:43 +0200

    Updated telescope to v0.1.8

--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -22,7 +22,7 @@ return require('packer').startup(
     }
 
     use { 'nvim-telescope/telescope.nvim',
-      tag = '0.1.4',
+      tag = '0.1.8',
       -- or                          , branch = '0.1.x',
       requires = { {'nvim-lua/plenary.nvim'} }
     }
