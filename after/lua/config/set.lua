812921726701574588f4175381bb84e35e1f2999
John-Philip Taylor <John-PhilipT@reutech.co.za>
2024-07-08 09:45:55 +0200

    Updated the commented-out tree-sitter folding functions

--- after/lua/config/set.lua
+++ after/lua/config/set.lua
@@ -25,7 +25,8 @@ vim.opt.shiftwidth    = 0
 vim.opt.expandtab     = true
 
 -- vim.opt.foldmethod    = 'expr'
--- vim.opt.foldexpr      = 'nvim_treesitter#foldexpr()'
+-- vim.opt.foldexpr      = 'v:lua.vim.treesitter.foldexpr()'
+-- -- vim.opt.foldtext      = 'v:lua.vim.treesitter.foldtext()'
 -- vim.opt.foldenable    = false
 
 vim.opt.foldmethod     = 'indent'
