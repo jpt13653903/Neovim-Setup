d73131516e016e5056269c6dfe1ba5ac6f9ea2e1
John-Philip Taylor <John-PhilipT@reutech.co.za>
2024-11-05 11:56:23 +0200

    Removed JSON file auto-folding on open

--- after/lua/config/autocmd.lua
+++ after/lua/config/autocmd.lua
@@ -5,9 +5,6 @@ vim.cmd.autocmd('FocusGained * silent! checktime')
 -- Remove trailing white-space on save
 vim.cmd.autocmd('BufWrite * silent! %s/\\s\\+$//g')
 
--- Fold JSON files to level 1
-vim.cmd.autocmd('BufRead *.json,*.json5 silent! set foldlevel=1')
-
 -- Disable folding in telescope results
 vim.cmd.autocmd('FileType TelescopeResults setlocal nofoldenable')
 
