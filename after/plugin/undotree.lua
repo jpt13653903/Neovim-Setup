9d8a1f0758b1b4a5c94760e0aa80b155c826c05b
John-Philip Taylor <jpt13653903@gmail.com>
2024-12-03 20:49:41 +0200

    Fixed the broken undotree plugin

--- after/plugin/undotree.lua
+++ after/plugin/undotree.lua
@@ -2,4 +2,6 @@ vim.keymap.set('n', '<leader>u', function()
   vim.cmd.UndotreeShow()
   vim.cmd.UndotreeFocus()
 end)
+-- Windows doesn't have "diff", but uses "FC" instead
+vim.g.undotree_DiffCommand = "FC"
 
