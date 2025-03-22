fb36a9aa69005d0d4e0b14691d81eb4dcb18f082
John-Philip Taylor <jpt13653903@gmail.com>
2025-03-22 12:18:57 +0200

    Changed Telescope to run on the latest stable instead of v0.1.8

--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -1,32 +1,32 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(
-  function(use)
+    function(use)
     use { 'wbthomason/packer.nvim' }
 
     -- use { 'folke/tokyonight.nvim',
-    --   config = function()
-    --     vim.cmd.colorscheme('tokyonight')
-    --     -- vim.cmd.colorscheme('tokyonight-night')
-    --     -- vim.cmd.colorscheme('tokyonight-storm')
-    --     -- vim.cmd.colorscheme('tokyonight-day')
-    --     -- vim.cmd.colorscheme('tokyonight-moon')
-    --   end
+    --     config = function()
+    --         vim.cmd.colorscheme('tokyonight')
+    --         -- vim.cmd.colorscheme('tokyonight-night')
+    --         -- vim.cmd.colorscheme('tokyonight-storm')
+    --         -- vim.cmd.colorscheme('tokyonight-day')
+    --         -- vim.cmd.colorscheme('tokyonight-moon')
+    --     end
     -- }
 
     use { 'catppuccin/nvim' }
     -- use { 'navarasu/onedark.nvim' }
 
     use { 'nvim-treesitter/nvim-treesitter',
-      run = ':TSUpdate'
+        run = ':TSUpdate'
     }
 
     use { 'nvim-treesitter/nvim-treesitter-context' }
 
     use { 'nvim-telescope/telescope.nvim',
-      tag = '0.1.8',
-      -- or                          , branch = '0.1.x',
-      requires = { {'nvim-lua/plenary.nvim'} }
+        -- tag = '0.1.8',
+        branch = '0.1.x',
+        requires = { 'nvim-lua/plenary.nvim' }
     }
 
     -- Deprecated: use InspectTree instead
@@ -35,8 +35,8 @@ return require('packer').startup(
     use { 'mbbill/undotree' }
 
     use {
-      'nvim-lualine/lualine.nvim',
-      requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
 
     use {
@@ -51,27 +51,27 @@ return require('packer').startup(
     }
 
     use {
-      'stevearc/oil.nvim',
-      config = function()
-        require("oil").setup({
-          default_file_explorer = false
-        })
-      end,
+        'stevearc/oil.nvim',
+        config = function()
+            require("oil").setup({
+                default_file_explorer = false
+            })
+        end,
     }
 
     -- use { 'VonHeikemen/lsp-zero.nvim',
-      -- branch = 'v3.x',
-      -- requires = {
-      --   -- Manage LSP servers from neovim
-      --   {'williamboman/mason.nvim'},
-      --   {'williamboman/mason-lspconfig.nvim'},
-      --   -- LSP Support
-      --   {'neovim/nvim-lspconfig'},
-      --   -- Autocompletion
-      --   {'hrsh7th/nvim-cmp'},
-      --   {'hrsh7th/cmp-nvim-lsp'},
-      --   {'L3MON4D3/LuaSnip'},
-      -- }
+    --     branch = 'v3.x',
+    --     requires = {
+    --         -- Manage LSP servers from neovim
+    --         {'williamboman/mason.nvim'},
+    --         {'williamboman/mason-lspconfig.nvim'},
+    --         -- LSP Support
+    --         {'neovim/nvim-lspconfig'},
+    --         -- Autocompletion
+    --         {'hrsh7th/nvim-cmp'},
+    --         {'hrsh7th/cmp-nvim-lsp'},
+    --         {'L3MON4D3/LuaSnip'},
+    --     }
     -- }
-  end
+    end
 )
