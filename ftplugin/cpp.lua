c172b59afe4cc882dacc3246eb8d82c18fab162c
John-Philip Taylor <John-PhilipT@reutech.co.za>
2026-09-15 12:15:36 +0200

    Added clangd lsp setup

--- ftplugin/cpp.lua
+++ ftplugin/cpp.lua
@@ -5,22 +5,38 @@ CommentKeymap([[\/\/]])
 
 vim.opt_local.tabstop = 4;
 
--- vim.lsp.start({
---     name = 'clangd',
---     cmd = {'clangd'},
---     root_dir = vim.fs.dirname(vim.fs.find({ '.clangd', '.git' }, { upward = true })[1]) or '.',
---     on_attach = function(client, bufnr)
---         client.server_capabilities.semanticTokensProvider = nil
---         vim.lsp.completion.enable(true, client.id, bufnr, {
---             autotrigger = false,
---             convert = function(item)
---                 return { abbr = item.label:gsub('%b()', '') }
---             end,
---         })
---     end,
--- })
---
--- vim.keymap.set('n', 'K', function()
---     vim.lsp.buf.hover { border = 'rounded', max_height = 25, max_width = 120 }
--- end)
+vim.lsp.start({
+    name = 'clangd',
+    cmd = {'clangd'},
+    root_dir = vim.fs.dirname(vim.fs.find({ '.clangd', '.git', 'compile_commands.json' }, { upward = true })[1]) or '.',
+    on_attach = function(client, bufnr)
+        client.server_capabilities.semanticTokensProvider = nil
+        vim.lsp.completion.enable(true, client.id, bufnr, {
+            autotrigger = false,
+            convert = function(item)
+                return { abbr = item.label:gsub('%b()', '') }
+            end,
+        })
+    end,
+})
+
+vim.keymap.set('n', 'K', function()
+    vim.lsp.buf.hover { border = 'rounded', max_height = 25, max_width = 120 }
+end)
+vim.keymap.set('n', 'gri', function()
+    vim.lsp.buf.definition()
+    vim.lsp.buf.definition()
+end)
+vim.keymap.set('n', 'grd', function()
+    vim.lsp.buf.definition()
+end)
+vim.keymap.set('n', 'grD', function()
+    vim.lsp.buf.declaration()
+end)
+vim.keymap.set('n', 'grr', function()
+    vim.lsp.buf.references()
+end)
+vim.keymap.set('n', 'grn', function()
+    vim.lsp.buf.rename()
+end)
 
